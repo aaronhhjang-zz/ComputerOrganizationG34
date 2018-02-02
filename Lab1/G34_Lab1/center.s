2d9e1af58c67f2a6087a0bfc742fd88a2932eafb
Kun Zheng <kun.zheng@mail.mcgill.ca>
2018-02-02 14:15:57 -0500

    Refactor center.s

--- Lab1/G34_Lab1/center.s
+++ Lab1/G34_Lab1/center.s
@@ -10,7 +10,6 @@ _start:
 			LDR R9, =AVG		//R9 points to the AVG
 			LDR R10, [R9]		//initialize average to 0
 			LDR R2, [R9]		//initialize R2 to 0
-			
 SUM_LOOP:	
 			SUBS R1, R1, #1		//decrement the loop counter
 			BLT SHIFT_COUNT		//end loop if counter has reached negative.
@@ -18,29 +17,20 @@ SUM_LOOP:
 			ADD R3, R3, #4		//R3 points to he next signal
 			LDR R0, [R3]		//update R0 with the next signal value
 			B SUM_LOOP			//back to the top of the loop
-
-
 SHIFT_COUNT:	
 			LSR R7, R7, #1		//shift right
 			ADDS R2, R2, #1 	//increment the shift counter
 			CMP R7, #1			//check if LSB is 1
 			BEQ SUM_DONE		//if equal to 0, do not increment couner and proceed
 			B SHIFT_COUNT		//go back to SHIFT_COUNT
-				
-
-
 SUM_DONE:
 			LSR R10, R10, R2	//R10 holds the new shifted (/N) value		
 			B AVG_DONE
-
-
 AVG_DONE:	
 			LDR R4, =N			//R4 points to the N location
 			LDR R1, [R4]		//R1 holds the number of signal
 			ADD R3, R4, #4 		//R3 points to the first signal
 			B SUB_LOOP
-
-
 SUB_LOOP:	
 			SUBS R1, R1, #1		//decrement the loop counter
 			BLT DONE			//end loop if counter has reached -
@@ -51,8 +41,6 @@ SUB_LOOP:
 			B SUB_LOOP			//repeat SUB_LOOP
 
 DONE: 		B DONE				//infinite loop!
-
-
 
 AVG:		.word 0				//memory assigneed for the average
 N:			.word 8				//number of entries in the list
